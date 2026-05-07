6a2cd5b5b66518e07adc5d0b7cdfbeca0190de54
NeZnayuDA <rogzi0011@gmail.com>
2026-05-07 12:30:55 +0500

    knopki rabat

--- firebase-config.js
+++ firebase-config.js
@@ -2,16 +2,18 @@
 // Using Firebase v9 compat mode (CDN)
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_TO3XD2Uj_p62UroAvA57igN-XD4wVrU",
-    authDomain: "trening-aura.firebaseapp.com",
-    databaseURL: "https://trening-aura-default-rtdb.firebaseio.com",
-    projectId: "trening-aura",
-    storageBucket: "trening-aura.firebasestorage.app",
-    messagingSenderId: "237244099863",
-    appId: "1:237244099863:web:3ffbcd47132da71b469b52",
-    measurementId: "G-DXZQG97TV1"
+    apiKey: "AIzaSyDSf3wSUxxjZMp4-AvcxKBoWpKMBZWCtZc",
+    authDomain: "trening-aura-2d26f.firebaseapp.com",
+    databaseURL: "https://trening-aura-2d26f-default-rtdb.firebaseio.com",
+    projectId: "trening-aura-2d26f",
+    storageBucket: "trening-aura-2d26f.firebasestorage.app",
+    messagingSenderId: "210768590346",
+    appId: "1:210768590346:web:83e36edb78599f77fb51f3",
+    measurementId: "G-41YHJ2FELV"
 };
 
 // firebase-config.js
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
+
+
